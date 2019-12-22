f84454c164c4a711dce165c71c153c5debfc496c
DESKTOP-D154ENL\Leil <leil1230@qq.com>
2019-12-22 22:06:42 +0800

    初始化仓库提交

---
+++ src/com/littlestones/JavaThreadDemo01.java
@@ -0,0 +1,20 @@
+package com.littlestones;
+
+class ThreadDemo extends Thread {
+    @Override
+    public void run() {
+        System.out.println("这里是子线程");
+        System.out.println("线程名称：" + this.getName());
+        System.out.println("线程ID：" + this.getId());
+    }
+}
+
+public class JavaThreadDemo01 {
+
+    public static void main(String[] args) {
+        System.out.println("这里是主线程");
+        ThreadDemo threadDemo = new ThreadDemo();
+        threadDemo.start();
+    }
+
+}
